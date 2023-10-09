dd1f691bfb40f702713dbd8bd1ef00f6903d0961
viniciuss <146454578+niermidnight@users.noreply.github.com>
2023-10-09 09:48:54 -0300

    Update PARTE05.js

--- PARTE05.js
+++ PARTE05.js
@@ -294,14 +294,14 @@ function loadItens() {
 }
 
 
-// EXPORTAÇAO
+/* EXPORTAÇAO
 function ExportToExcel(type, fn, dl) {
   var elt = document.getElementById('resultado');
   var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
   return dl ?
     XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
     XLSX.writeFile(wb, fn || ('Catalogo de sistemas.' + (type || 'xlsx')));
-}
+} */
 
 
 loadAndSortItens()
